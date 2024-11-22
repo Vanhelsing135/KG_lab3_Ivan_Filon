ca30e7842f53314ff5cfba68338d5cf03c69406c
Ivan Filon <vanbest27@gmail.com>
2024-11-22 14:14:28 +0300

    Revert "Fixed and done"
    This reverts commit 3e1938e22b01cd8e3e09f0410c64d92d4ff7af5f.

--- src/Algorithms.java
+++
@@ -1,6 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-
-public interface Algorithms {
-    void draw(Graphics g, int r, int x0, int y0, int x1, int y1, JTextArea logArea, int cellSize, int centerX, int centerY);
-}
